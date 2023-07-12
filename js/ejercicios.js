47c7b8319ec2a05c891bb2fdeab4358da2e5380b
Susana <susoto0916@gmail.com>
2023-07-11 23:45:30 -0600

    Se agrega practica de control de funciones

---
+++ js/ejercicios.js
@@ -0,0 +1,39 @@
+
+const multiplica =function(a,b){
+
+    return a*b;
+    //multiplica
+    }
+    const suma3 =(a,b,c) =>(a+b+c);
+    
+    //hoisting
+    console.log(suma(45,13));
+    console.log(resta(45,13));
+    console.log("multiplica 3x6:", multiplica(3,6));
+    console.log("suma 3 numeros:(5,48,79):", suma(5,48,79));
+    console.log(ejercicio1(20,3));
+    
+    function suma(a,b){
+        return a+b;}
+        function resta(x,y){
+    
+            return x-y;
+        }
+    const suma3
+    
+    let multiplica = function(a,b){
+    return a*b;
+    }
+    
+    function resta(x,y){
+    
+    return (x-y);
+    
+    }
+    function suma(a,b){
+    
+    let c=a+b;
+    
+    }
+    
+    
---
+++ js/main.js
@@ -0,0 +1,25 @@
+//ejercicio 1
+function calcularCosto(payment) {
+return(payment+3)*1.01;
+}
+console.log(calculatefee(20));
+
+
+
+//ejercico2
+    functionpromtnames(name1,name2,name3){
+        console.log("welcome"+name1+","+name2+","+name3+".");}
+
+        prompname("alex","hina","luis")
+        function calcularage(year){
+let today =new Date();
+return(today.getFullYear()-year);
+        }
+
+        console.log(calculateage(1998));
+        function prompnameage(name1,year1, name2, year2, name3, year3){
+
+            console.log("welcoem"+calculateage(year2)+","+ name3+""+calculateage(year3)+".");
+
+        }
+        PerformanceEntry
